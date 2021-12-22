3c0f2bcac596366a0b1958ef8d97b4c2096ff436
Sandesh Singh <sandesh1014@gmail.com>
2021-12-22 20:19:42 +0530

    first button added

--- rn/rn-starter/src/screens/HomeScreen.js
+++ rn/rn-starter/src/screens/HomeScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View,Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World</Text>;
+  return <View>
+    <Text style={styles.text}>Hello World</Text>
+    <Button 
+      onPress={()=>console.log('USer interaction!')}
+      title="Button 1!"/>    
+    </View>
 };
-
+//Self closing button
+//Button look depends on OS
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
