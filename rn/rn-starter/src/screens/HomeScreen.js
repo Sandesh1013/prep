d2318d96516b514865000500c64153de6ff34066
Sandesh Singh <sandesh1014@gmail.com>
2021-12-22 21:17:41 +0530

    Cleaning up code

--- rn/rn-starter/src/screens/HomeScreen.js
+++ rn/rn-starter/src/screens/HomeScreen.js
@@ -5,14 +5,14 @@ const HomeScreen = (props) => {
   return <View>
     <Button 
       onPress={()=>props.navigation.navigate('Components')}
-      title="Button 2!"/>   
-    <TouchableOpacity onPress={()=> console.log(props)}>
-      <Text>Button 2!</Text>  
-    </TouchableOpacity> 
+      title="To components"/>   
+    <Button 
+      onPress={()=>props.navigation.navigate('List')} 
+      title='To list'/>
     </View>
 };
 //Self closing button
-//Button look depends on OS
+//Button look depends on OS, less configurable though
 // Touchable Opacity to detect, it is an attribute, maybe more useful than a button!
 const styles = StyleSheet.create({
   text: {
