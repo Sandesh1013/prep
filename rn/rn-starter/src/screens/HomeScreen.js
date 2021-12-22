8ae6ee9494e2ceabaf2b4811f9dd73d55d503319
Sandesh Singh <sandesh1014@gmail.com>
2021-12-22 21:10:50 +0530

    Added basic navigation method via props

--- rn/rn-starter/src/screens/HomeScreen.js
+++ rn/rn-starter/src/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Text, StyleSheet, View,Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return <View>
     <Button 
-      onPress={()=>console.log('USer interaction!')}
+      onPress={()=>props.navigation.navigate('Components')}
       title="Button 2!"/>   
-    <TouchableOpacity onPress={()=> console.log('Second button pressed')}>
+    <TouchableOpacity onPress={()=> console.log(props)}>
       <Text>Button 2!</Text>  
     </TouchableOpacity> 
     </View>
 };
 //Self closing button
 //Button look depends on OS
-// Touchable Opacity to detect, it is an attribute, mayne more used than a button!
+// Touchable Opacity to detect, it is an attribute, maybe more useful than a button!
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
